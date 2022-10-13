7134ad0cf0a2aa39f89741c17bdfedd31aaed6dc
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-10-13 16:22:34 +0530

    Dart Tutorial 8_Inheritence

---
+++ Day 15 of 100dayscodechallenge_Dart Tutorial 8_Inheritence/dartTuitorial_8_Inheritence.dart
@@ -0,0 +1,50 @@
+//Date 13/10/2022 03:24 PM
+
+//Dart programming fundamentals
+//Topics covered is Inheritence
+
+void main() {
+
+	var dog = Dog();
+	dog.breed = "Labrador";
+	dog.color = "Black";
+	dog.bark();
+	dog.eat();
+
+	var cat = Cat();
+	cat.color = "White";
+	cat.age = 6;
+	cat.eat();
+	cat.meow();
+
+	var animal = Animal();
+	animal.color = "brown";
+	animal.eat();
+}
+
+class Animal {
+
+	String color="";
+
+	void eat() {
+		print("Eat !");
+	}
+}
+
+class Dog extends Animal {      // Dog is Child class or sub class, Animal is super or parent class
+
+	String breed="";
+
+	void bark() {
+		print("Bark !");
+	}
+}
+
+class Cat extends Animal {      // Cat is Child class or sub class, Animal is super or parent class
+
+	int age=0;
+
+	void meow() {
+		print("Meow !");
+	}
+}
