8215d940bdf7afbb63d186fbf167dfaf1d35ac09
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-10-14 18:33:11 +0530

    dartTuitorial_9_Method Overriding

---
+++ Day 16 of 100dayscodechallenge_Dart Tutorial 9_Method Overriding/dartTuitorial_9_Method Overriding.dart
@@ -0,0 +1,39 @@
+//Date 14/10/2022 06:00 PM
+
+//Dart programming fundamentals
+//Topics covered is Method Overriding
+
+void main() {
+
+	var dog = Dog();
+	dog.eat();
+
+	print(dog.color);
+}
+
+class Animal {
+
+	String color = "brown";
+
+	void eat() {
+		print("Animal is eating !");
+	}
+}
+
+class Dog extends Animal {
+
+	String breed="";
+
+	String color = "Black";     // Property Overriding
+
+	void bark() {
+		print("Bark !");
+	}
+
+	// Method Overriding
+	void eat() {
+		print("Dog is eating !");
+		super.eat();
+		print("More food to eat");
+	}
+}
