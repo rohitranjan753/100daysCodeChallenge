06d423ace16054960dc5f5c6946ef3f7bba00642
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-10-18 18:50:46 +0530

    dartTuitorial_13_Static Methods & Variables

---
+++ Day 20 of 100dayscodechallenge_Dart Tutorial 13_Static Methods and variables/dartTuitorial_13_Static Methods & Variables.dart
@@ -0,0 +1,46 @@
+//Date 18/10/2022 06:20 PM
+
+//Dart programming fundamentals
+//Topics covered is Static methods and variables
+
+void main() {
+
+	var circle1 = Circle();
+//	circle1.pi;     // 4 bytes
+
+	var circle2 = Circle();
+//	circle2.pi;     // 4 bytes
+
+	// 8 bytes      // waste of extra 4 bytes
+
+	Circle.pi;  // 4 bytes
+	Circle.pi;  // No more memory will be allocated .
+
+
+//	circle.calculateArea();
+
+//	print(Circle.pi);           // Syntax to call Static Variable
+
+//	Circle.calculateArea();     // Syntax to call Static Method
+}
+
+class Circle {
+
+	static const double pi = 3.14;
+	static int maxRadius = 5;
+
+	String color="image.png";
+
+	static void calculateArea() {
+		print("Some code to calculate area of Circle");
+//		myNormalFunction();     // Not allowed to call instance functions
+//		this.color;             // You cannot use 'this' keyword and even cannot access Instance variables
+	}
+
+	void myNormalFunction() {
+		calculateArea();
+		this.color = "Red";
+		print(pi);
+		print(maxRadius);
+	}
+}
