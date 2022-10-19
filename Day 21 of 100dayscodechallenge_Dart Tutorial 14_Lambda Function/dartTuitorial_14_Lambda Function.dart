09abec6829b24c469636f6914e1ddd191f774057
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-10-19 18:58:52 +0530

    dartTuitorial_14_Lambda Function

---
+++ Day 21 of 100dayscodechallenge_Dart Tutorial 14_Lambda Function/dartTuitorial_14_Lambda Function.dart
@@ -0,0 +1,38 @@
+//Date 19/10/2022 06:20 PM
+
+//Dart programming fundamentals
+//Topics covered is Lambda function
+
+void main() {
+
+	// Defining Lambda: 1st way
+	Function addTwoNumbers = (int a, int b) {
+		var sum = a + b;
+		print(sum);
+	};
+
+	var multiplyByFour = (int number) {
+		return number * 4;
+	};
+
+	// Defining Lambda: 2nd way: Function Expression: Using Short Hand Syntax or FAT Arrow ( '=>' )
+	Function addNumbers = (int a, int b) => print(a + b);
+
+	var multiplyFour = (int number) => number * 4;
+
+
+	// Calling lambda function
+	addTwoNumbers(2, 5);
+	print(multiplyByFour(5));
+
+	addNumbers(3, 7);
+	print(multiplyFour(10));
+}
+
+
+// A example of Normal function
+void addMyNumbers(int a, int b) {
+
+	var sum = a + b;
+	print(sum);
+}
