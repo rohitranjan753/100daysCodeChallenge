1aeaddad9adeebc92f55c69622d01cd8c9be29ad
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-10-28 16:22:07 +0530

    dartTutorial_23_Isolates

---
+++ Day 30 of 100dayscodechallenge_Dart Tutorial 23_Isolates/dartTutorial_23_Isolates.dart
@@ -0,0 +1,18 @@
+//Date 28/10/2022 04:15 PM
+
+//Dart programming fundamentals
+//Topics covered is Isolate
+
+import 'dart:isolate';    
+void sayhii(var msg){   
+   print('execution from sayhii ... the message is :${msg}');   
+}    
+void main(){   
+   Isolate.spawn(sayhii,'Hello!!');   
+   Isolate.spawn(sayhii,'Whats up!!');   
+   Isolate.spawn(sayhii,'Welcome!!');   
+     
+   print('execution from main1');   
+   print('execution from main2');   
+   print('execution from main3');   
+}
