03121b93811de37428391cefe20ea9dede99bfb2
Rohit Ranjan <57181495+rohitranjan753@users.noreply.github.com>
2022-12-10 20:53:06 +0530

    flutterTutorial_35_Day35_Lists

---
+++ Day 72 of 100dayscodechallenge_Flutter Tutorial 35_Day35_Lists/flutterTutorial_35_Day35_Lists.dart
@@ -0,0 +1,51 @@
+//Date 10/12/2022 08:00 PM
+
+//Flutter Learning Day 35.
+//Topics covered is Lists.
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Flutter List';
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+          backgroundColor: Colors.red,
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text('Contact'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Setting'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
